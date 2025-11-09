3dc82dc7f78b124645d50618b4f7eb69c53d80df
Shem Palao <raxor102106@gmail.com>
2025-11-09 12:15:28 +0800

    add solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    return array.filter((value) => (!numbers.includes(value)));
 };
 
 // Do not edit below this line
